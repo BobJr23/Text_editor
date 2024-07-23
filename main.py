1fea83f315a5354b0564d2f22af3d0962cf4755a
Andrew Ma <ama25@belmontschools.net>
2024-07-23 09:09:09 -0400

    file selection mode and bug fixes

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def main():
 
     layout = [
         [sg.Menu([["File", ["Open", "Save", "Save As", "Close file", "Exit"]]])],
-        [],
+        [sg.Listbox(values=[], enable_events=True, size=(None, 3), key="listbox", expand_x=True, sbar_width=3,
+                    sbar_arrow_width=3, sbar_trough_color=None,)],
         [sg.Multiline("Choose a file to begin", key="text", expand_x=True, expand_y=True)],
     ]
 
@@ -28,6 +29,10 @@ def main():
                     text = f.read()
                     window["text"].update(text)
                     f.close()
+                current_files = window["listbox"].get_list_values()
+
+                current_files.append(filename)
+                window["listbox"].update(current_files)
 
             case "Save":
                 with open(filename, "w") as f:
@@ -44,8 +49,20 @@ def main():
                 )
 
             case "Close file":
-                filename = None
+
+                current_files = window["listbox"].get_list_values()
+
+                current_files.pop(current_files.index(values["listbox"][0]))
+                window["listbox"].update(current_files)
                 window["text"].update("")
+
+            case "listbox":
+                filename = values["listbox"][0]
+                if filename != "":
+                    with open(filename) as f:
+                        text = f.read()
+                        window["text"].update(text)
+                        f.close()
 
             case "Exit":
                 break
