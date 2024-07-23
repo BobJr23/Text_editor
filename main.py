681be199e96527744cc52c947e0c2cfac9024c55
Andrew Ma <ama25@belmontschools.net>
2024-07-23 10:09:24 -0400

    theme editor and previewer

--- main.py
+++ main.py
@@ -4,14 +4,37 @@ import os
 
 def main():
     filename = None
-    sg.theme('Darkblue')
-    sg.set_options(font=('Courier New', 15))
+    sg.theme("Darkblue")
+    sg.set_options(font=("Courier New", 15))
 
     layout = [
-        [sg.Menu([["File", ["Open", "Save", "Save As", "Close file", "Exit"]]])],
-        [sg.Listbox(values=[], enable_events=True, size=(None, 3), key="listbox", expand_x=True, sbar_width=3,
-                    sbar_arrow_width=3, sbar_trough_color=None,)],
-        [sg.Multiline("Choose a file to begin", key="text", expand_x=True, expand_y=True)],
+        [
+            sg.Menu(
+                [
+                    [
+                        "File",
+                        ["Open", "Save", "Save As", "Close file", "---", "Exit editor"],
+                    ],
+                    ["Theme", ["Preview themes", "Choose theme"]],
+                ]
+            )
+        ],
+        [
+            sg.Listbox(
+                values=[],
+                enable_events=True,
+                size=(None, 3),
+                key="listbox",
+                expand_x=True,
+                sbar_width=3,
+                sbar_arrow_width=3,
+            )
+        ],
+        [
+            sg.Multiline(
+                "Choose a file to begin", key="text", expand_x=True, expand_y=True
+            )
+        ],
     ]
 
     window = sg.Window("BobJr editor", layout, resizable=True).Finalize()
@@ -25,14 +48,15 @@ def main():
 
             case "Open":
                 filename = sg.popup_get_file("Select a file", no_window=True)
-                with open(filename) as f:
-                    text = f.read()
-                    window["text"].update(text)
-                    f.close()
-                current_files = window["listbox"].get_list_values()
+                if filename is not None:
+                    with open(filename) as f:
+                        text = f.read()
+                        window["text"].update(text)
+                        f.close()
+                    current_files = window["listbox"].get_list_values()
 
-                current_files.append(filename)
-                window["listbox"].update(current_files)
+                    current_files.append(filename)
+                    window["listbox"].update(current_files)
 
             case "Save":
                 with open(filename, "w") as f:
@@ -43,9 +67,13 @@ def main():
                 with open(filename, "w") as f:
                     f.write(values["text"])
                     f.close()
-                os.rename(filename, sg.popup_get_file(
-                    "Select a file", save_as=True, no_window=True,
-                    )
+                os.rename(
+                    filename,
+                    sg.popup_get_file(
+                        "Select a file",
+                        save_as=True,
+                        no_window=True,
+                    ),
                 )
 
             case "Close file":
@@ -57,14 +85,61 @@ def main():
                 window["text"].update("")
 
             case "listbox":
-                filename = values["listbox"][0]
-                if filename != "":
-                    with open(filename) as f:
-                        text = f.read()
-                        window["text"].update(text)
-                        f.close()
+                try:
+                    filename = values["listbox"][0]
+                except IndexError:
+                    continue
+                with open(filename) as f:
+                    text = f.read()
+                    window["text"].update(text)
+                    f.close()
 
-            case "Exit":
+            case "Preview themes":
+                sg.theme_previewer(scrollable=True, columns=6)
+
+            case "Choose theme":
+                window.disable()
+                theme = None
+                window2 = sg.Window(
+                    "Choose a theme",
+                    [
+                        [sg.Input(key="theme-input", enable_events=True, size=(20, 1))],
+                        [
+                            sg.Listbox(
+                                values=sg.theme_list(),
+                                key="theme",
+                                expand_y=True,
+                                expand_x=True,
+                                enable_events=True,
+                            )
+                        ],
+                        [sg.Button("Submit")],
+                    ],
+                    resizable=True,
+                ).Finalize()
+                window2.maximize()
+                while True:
+                    event2, values2 = window2.read()
+                    print(event2, values2)
+                    if event2 in (sg.WIN_CLOSED, "Submit"):
+                        break
+                    if event2 == "theme":
+                        theme = values2["theme"][0]
+                        sg.theme(theme)
+
+                    if event2 == "theme-input":
+                        window2["theme"].update(
+                            [
+                                i
+                                for i in sg.theme_list()
+                                if values2["theme-input"].lower() in i.lower()
+                            ]
+                        )
+
+                window2.close()
+                window.enable()
+
+            case "Exit editor":
                 break
 
     window.close()
