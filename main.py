c7ff07acfd97b8ef687f4a44f3a84ee716078d58
Andrew Ma <ama25@belmontschools.net>
2024-07-26 16:34:39 -0400

    supporting custom themes and refactoring

--- main.py
+++ main.py
@@ -1,40 +1,11 @@
-import PySimpleGUI as sg
+# import PySimpleGUI as sg
 import json
 import re
 from highlighter import do_highlighting, styles
 import os
 from run_file import run
 import threading
-
-
-# modified https://github.com/PySimpleGUI/PySimpleGUI/issues/2437
-def update_theme(window: sg.Window, selected_theme):
-    if selected_theme is None:
-        return
-    current_them = sg.LOOK_AND_FEEL_TABLE[selected_theme]
-
-    try:
-        window_bkg = current_them.get("BACKGROUND")
-        window.TKroot.config(background=window_bkg)
-    except Exception as e:
-        print(e)
-
-    for v, element in window.AllKeysDict.items():
-        try:
-            if element.Type == "button":
-                color = current_them.get("BUTTON")
-                element.Widget.config(foreground=color[0], background=color[1])
-            elif element.Type in ["listbox", "multiline", "input"]:
-                color = current_them.get("INPUT")
-                element.Widget.config(
-                    background=color, foreground=current_them.get("TEXT_INPUT")
-                )
-            else:
-                color = current_them.get("BACKGROUND")
-                element.Widget.config(background=color)
-            element.update()
-        except Exception as e:
-            print(e)
+from theme_maker import theme_selector, sg
 
 
 def load_from_file(filename, window: sg.Window, tab_dict, add_to_tab=True, update=True):
@@ -566,62 +537,7 @@ def main():
                     ).start()
 
             case "Choose theme":
-                window.disable()
-                theme = None
-                window2 = sg.Window(
-                    "Choose a theme",
-                    [
-                        [
-                            sg.Input(
-                                focus=True,
-                                tooltip="Search for a theme",
-                                key="theme-input",
-                                enable_events=True,
-                                size=(20, 1),
-                            )
-                        ],
-                        [
-                            sg.Listbox(
-                                values=sg.theme_list(),
-                                key="theme",
-                                size=(20, 20),
-                                expand_y=True,
-                                enable_events=True,
-                            ),
-                            sg.Text("Example Text", size=(20, 1)),
-                            sg.Button("Example Button, size=(20, 1)"),
-                            sg.Input(default_text="Example Input", size=(20, 1)),
-                            sg.Multiline("Example Multiline", size=(20, 20)),
-                        ],
-                        [sg.Button("Submit")],
-                    ],
-                    resizable=True,
-                    finalize=True,
-                )
-                window2.maximize()
-
-                while True:
-                    event2, values2 = window2.read()
-
-                    if event2 in (sg.WIN_CLOSED, "Submit"):
-                        break
-                    if event2 == "theme":
-                        theme = values2["theme"][0]
-                        update_theme(window2, theme)
-
-                    if event2 == "theme-input":
-                        window2["theme"].update(
-                            [
-                                i
-                                for i in sg.theme_list()
-                                if values2["theme-input"].lower() in i.lower()
-                            ]
-                        )
-
-                window2.close()
-
-                window.enable()
-                update_theme(window, theme)
+                theme_selector(window)
             case "Exit editor":
                 break
     print(save_folder)
